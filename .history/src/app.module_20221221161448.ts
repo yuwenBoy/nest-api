2d4fb6d5c783305ef4b53a78e187c2ddb4fde7f7
zhaojian <zhaojian@eltc.cn>
2022-12-21 16:39:45 +0800

    .history文件删除

--- .history/src/app.module_20221221161448.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HelloModule } from './hello/hello.module';
-
-@Module({
-  imports: [HelloModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
