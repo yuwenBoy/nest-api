7a913bb60318714e4d4880fe718a5b0c7c232d28
zhaojian <zhaojian@eltc.cn>
2023-03-30 14:11:58 +0800

    修改token一小时过期，刷新token2小时过期

--- src/modules/common/collections-permission/constants/jwtContants.ts
+++ src/modules/common/collections-permission/constants/jwtContants.ts
@@ -3,6 +3,6 @@
  */
 export const jwtContants = {
   secret: 'json_web_token_secret_key',
-  expiresIn:'60s', // token过期时间 默认值1天  d天后过期 s秒后过期
+  expiresIn:'1h', // token过期时间 默认值1天  d天后过期 s秒后过期
 };
       
